64e31514c9b6fe1eac2d21c5c14cf79124dcb71a
gabombp <gazofeifa@mpbsystemlogic>
2025-02-21 21:20:00 -0600

    gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  webAPI1: "https://betslipapi.5itedev.com/api/",
-  webAPI2:  "https://betslipapi.5itedev.com/api/",
+  webAPI1: "https://coreapi.nexuswager.com/api/",
+  webAPI2:  "https://coreapi.nexuswager.com/api/",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,39 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
- //webAPI1: "http://betslipapi.5itedev.com/api/",
- webAPI1: "http://10.0.0.53/wagercorewebapi/api/",
- webAPI2: "http://10.0.0.53/wagercorewebapi/api/",
-  // webAPI1: "https://betslipapi.5itedev.com/api/",
-  // webAPI2:  "https://betslipapi.5itedev.com/api/",
-
-   //  webAPI1: "http://10.0.0.53/wagercorewebapi/api/",
-  //webAPI2: "http://10.211.55.3/DotNetFxAPI/api/",
+  webAPI1: "https://coreapi.nexuswager.com/api/",
+  webAPI2:  "https://coreapi.nexuswager.com/api/",
 };
-
-/*
-
-  webAPI1: "https://apibs.cryptogaming.bet/api/",
-  //webAPI1: "http://10.211.55.3/DotNetFxAPI/api/",
-  webAPI2: "https://api.gomobilewager.com/api/"
-
-
-  webAPI1: "http://10.211.55.4/WagerCoreWebAPI/api/",
-http://10.211.55.4:5051/
-  webAPI1: "http://localhost:54986/api/",
- webAPI2: "https://api.gomobilewager.com/api/"
-  webAPI2: "http://10.211.55.3:5050/api/"
-
- webAPI1: "http://10.211.55.3:5050/api/"
-  webAPI1: "http://10.211.55.3/DotNetFxAPI/api/",
-  webAPI1: "https://localhost:44330/api/",
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
